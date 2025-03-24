df71f5f7502b1340e07f31efb4f93d6be725c633
andrew <akberger20@gmail.com>
2025-03-24 16:26:48 -0500

    adding http generation response logic

--- toi_server/src/routes/assist.rs
+++ toi_server/src/routes/assist.rs
@@ -1,4 +1,5 @@
 use axum::{
+    body::Body,
     extract::{Path, Query, State},
     http::StatusCode,
     response::{Json, Response},
@@ -23,7 +24,7 @@ pub fn router(state: models::state::ToiState) -> OpenApiRouter {
 async fn chat(
     State(state): State<models::state::ToiState>,
     Json(request): Json<models::client::GenerateRequest>,
-) -> () {
+) -> Result<Body, (StatusCode, String)> {
     // First step is classifying the type of response most appropriate based on the
     // user's chat history and last message.
     let chat_response_kind_system_prompt = chat_response_kind_system_prompt(state.openapi_spec);
@@ -32,24 +33,56 @@ async fn chat(
         content: chat_response_kind_system_prompt,
     }];
     chat_response_kind_messages.extend_from_slice(&request.messages);
-    let chat_response_kind_request = models::client::GenerateRequest {
+    let chat_response_kind_generation_request = models::client::GenerateRequest {
         messages: chat_response_kind_messages,
     };
-    let chat_response_kind = state.client.generate(chat_response_kind_request).await?;
+    let chat_response_kind = state
+        .client
+        .generate(chat_response_kind_generation_request)
+        .await?;
     let chat_response_kind = chat_response_kind.parse::<u8>()?;
     let chat_response_kind: models::assist::ChatResponseKind = chat_response_kind.into();
 
     // Map the response kind to different prompts and different ways for constructing
     // the final response.
-    let chat_response_system_prompt =
-        chat_response_http_system_prompt(state.openapi_spec, chat_response_kind);
     match chat_response_kind {
         models::assist::ChatResponseKind::Unfulfillable
         | models::assist::ChatResponseKind::FollowUp
         | models::assist::ChatResponseKind::Answer
-        | models::assist::ChatResponseKind::AnswerWithDraftHttpRequests => {}
+        | models::assist::ChatResponseKind::AnswerWithDraftHttpRequests => {
+            let chat_response_system_prompt =
+                chat_response_system_prompt(state.openapi_spec, chat_response_kind);
+            let mut chat_response_messages = vec![models::client::Message {
+                role: models::client::MessageRole::System,
+                content: chat_response_system_prompt,
+            }];
+            chat_response_messages.extend_from_slice(&request.messages);
+            let chat_response_generation_request = models::client::GenerateRequest {
+                messages: chat_response_messages,
+            };
+            let stream = state
+                .client
+                .generate_stream(chat_response_generation_request)
+                .await?;
+            Ok(stream)
+        }
         models::assist::ChatResponseKind::PartiallyAnswerWithHttpRequests
-        | models::assist::ChatResponseKind::AnswerWithHttpRequests => {}
+        | models::assist::ChatResponseKind::AnswerWithHttpRequests => {
+            let chat_response_system_prompt =
+                chat_response_http_system_prompt(state.openapi_spec, chat_response_kind);
+            let mut chat_response_messages = vec![models::client::Message {
+                role: models::client::MessageRole::System,
+                content: chat_response_system_prompt,
+            }];
+            chat_response_messages.extend_from_slice(&request.messages);
+            let http_requests_generation_request = models::client::GenerateRequest {
+                messages: chat_response_messages,
+            };
+            let http_requests = state
+                .client
+                .generate(http_requests_generation_request)
+                .await?;
+        }
     }
 }
 
